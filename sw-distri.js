10f4e34eff4540f282589ba0e68b3b976d1c7751
Cybernetsp <cyber@cybernetsp.com>
2026-06-17 09:12:31 -0500

    Update sw-distri.js

--- sw-distri.js
+++ sw-distri.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "cyber-distri-v9"; // 🔄 Cambiamos a V2 para forzar al celular a borrar el error anterior
+const CACHE_NAME = "cyber-distri-v10"; // 🔄 Cambiamos a V2 para forzar al celular a borrar el error anterior
 
 const assetsToCache = [
   "./distribuidores",
