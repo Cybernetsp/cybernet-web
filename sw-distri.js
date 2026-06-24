31aaccbc5dc9ad5b91a470831941304590d20ca4
Cybernetsp <cyber@cybernetsp.com>
2026-06-24 14:27:50 -0500

    Update service worker cache version to v12

--- sw-distri.js
+++ sw-distri.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "cyber-distri-v11"; // 🔄 Cambiamos a V2 para forzar al celular a borrar el error anterior
+const CACHE_NAME = "cyber-distri-v12"; // 🔄 Cambiamos a V2 para forzar al celular a borrar el error anterior
 
 const assetsToCache = [
   "./distribuidores",
