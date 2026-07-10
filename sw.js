2a40a4544520ced972864a37e7c82393e8b57c0d
Cybernetsp <cyber@cybernetsp.com>
2026-07-09 20:51:17 -0500

    Increment cache version to v37

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "cybernet-cache-v36"; // Súbele una versión más por si acaso
+const CACHE_NAME = "cybernet-cache-v37"; // Súbele una versión más por si acaso
 
 const assets = ["./"];
 
