0f2c0f1eed69ac17153bac53a344d51b85f2b80e
Cybernetsp <cyber@cybernetsp.com>
2026-07-26 14:19:13 -0500

    Update CACHE_NAME to version v57
    Increment cache version to force update.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "cybernet-cache-v56"; // Subimos la versión para que fuerce la actualización
+const CACHE_NAME = "cybernet-cache-v57"; // Subimos la versión para que fuerce la actualización
 
 // ⚡ ARRAY COMBINADO: Aquí guardamos los archivos de TODAS tus páginas
 const assets = [
