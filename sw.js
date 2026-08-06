d14465642de3ddac9fa14a85fb63aba00818bb18
Cybernetsp <cyber@cybernetsp.com>
2026-08-06 10:15:36 -0500

    Update CACHE_NAME to version v73
    Increment cache version to force update.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "cybernet-cache-v72"; // Subimos la versión para que fuerce la actualización
+const CACHE_NAME = "cybernet-cache-v73"; // Subimos la versión para que fuerce la actualización
 
 // ⚡ ARRAY COMBINADO: Aquí guardamos los archivos de TODAS tus páginas
 const assets = [
