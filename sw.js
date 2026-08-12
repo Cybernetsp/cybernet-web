78529978a170c7e0bbe870f79f8a8b3f4b346ab8
Cybernetsp <cyber@cybernetsp.com>
2026-08-11 22:48:39 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "cybernet-cache-v87"; // Subimos la versión para que fuerce la actualización
+const CACHE_NAME = "cybernet-cache-v88"; // Subimos la versión para que fuerce la actualización
 
 // ⚡ ARRAY COMBINADO: Aquí guardamos los archivos de TODAS tus páginas
 const assets = [
